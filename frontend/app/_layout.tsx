dbd69708a1c777b2898d9dd1599124c132f56595
hyphsworld <hyphsworld3d@gmail.com>
2026-08-29 09:56:33 -0700

    Use platform-specific root shell

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,12 +1,3 @@
-import { Stack } from "expo-router";
-import { Platform } from "react-native";
-
 import NativeRoot from "@/src/components/NativeRoot";
 
-export default function RootLayout() {
-  if (Platform.OS === "web") {
-    return <Stack screenOptions={{ headerShown: false }} />;
-  }
-
-  return <NativeRoot />;
-}
+export default NativeRoot;
