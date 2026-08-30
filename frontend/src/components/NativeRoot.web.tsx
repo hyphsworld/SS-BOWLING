23cc01cebe83755f4a23468dd4aed243c78b9ac1
hyphsworld <hyphsworld3d@gmail.com>
2026-08-29 17:56:37 -0700

    Restore Super Strike audio initialization on web

--- frontend/src/components/NativeRoot.web.tsx
+++ frontend/src/components/NativeRoot.web.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { initAudio } from "@/src/audio/sounds";
 
 export default function NativeRootWeb() {
+  useEffect(() => {
+    initAudio();
+  }, []);
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
