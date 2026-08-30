98db668e761f210cc002c3411e9234c6bc604171
hyphsworld <hyphsworld3d@gmail.com>
2026-08-30 12:14:36 -0700

    Wire hazard overlay into Super Strike web root

--- frontend/src/components/NativeRoot.web.tsx
+++ frontend/src/components/NativeRoot.web.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
 import { initAudio } from "@/src/audio/sounds";
+import LaneHazardOverlay from "@/src/components/LaneHazardOverlay.web";
 
 export default function NativeRootWeb() {
   useEffect(() => {
     initAudio();
   }, []);
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }} />
+      <LaneHazardOverlay />
+    </>
+  );
 }
